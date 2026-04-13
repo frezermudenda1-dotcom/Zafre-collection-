653afa35f32271d438b82be9f503d53245b7bfe8
frezermudenda1-dotcom <frezermudenda1@gmail.com>
2026-04-13 21:45:56 +0200

    Update script.js

--- script.js
+++ script.js
@@ -3,4 +3,11 @@ let count = 0;
 function addToCart() {
     count++;
     document.getElementById("cart-count").innerText = count;
+
+    let popup = document.getElementById("popup");
+    popup.classList.add("show");
+
+    setTimeout(() => {
+        popup.classList.remove("show");
+    }, 1500);
 }
